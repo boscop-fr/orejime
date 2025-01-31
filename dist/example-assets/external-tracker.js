f2085761406d003ab1e1d1799b0881e0180d3c6a
Félix Girault <felix.girault@gmail.com>
2025-01-31 14:50:33 +0100

    Rebranded demo page

--- dist/example-assets/external-tracker.js
+++
@@ -1,3 +0,0 @@
-console.log("This is an example of an external tracking script.")
-//we set a tracking cookie as an example
-setCookie("external-tracker", "foo", 120)
---
+++ site/features/grouping.js
@@ -0,0 +1,19 @@
+window.orejimeConfig = {
+	purposes: [
+		{
+			id: 'group',
+			title: 'Analytics',
+			purposes: [
+				{
+					id: 'matomo',
+					title: 'Matomo'
+				},
+				{
+					id: 'google-analytics',
+					title: 'Google Analytics',
+				}
+			]
+		}
+	],
+	privacyPolicyUrl: '#'
+};
