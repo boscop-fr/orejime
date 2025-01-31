50325afd1b9019738cb228a12cac30717cc8df14
Félix Girault <felix.girault@gmail.com>
2025-01-31 14:50:33 +0100

    Rebranded demo page

---
+++ site/features/styling.js
@@ -0,0 +1,15 @@
+window.orejimeConfig = {
+	privacyPolicyUrl: '#',
+	purposes: [
+		{
+			id: 'mandatory',
+			title: 'Technical cookies',
+			description: 'Cookies required for the website to function'
+		},
+		{
+			id: 'analytics',
+			title: 'Analytics',
+			description: 'Cookies used to track user navigation'
+		}
+	]
+};
